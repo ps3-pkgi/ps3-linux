f06b35e0e0050580fce1503b6cc499eeade7b991
Geoff Levand <geoffrey.levand@am.sony.com>
2008-08-20 08:15:09 -0700

    [PS3] Merge in Geert's relocatable-fix-ppc32.diff

--- arch/powerpc/kernel/prom.c
+++ arch/powerpc/kernel/prom.c
@@ -1163,9 +1163,13 @@ void __init early_init_devtree(void *params)
 	parse_early_param();
 
 	/* Reserve LMB regions used by kernel, initrd, dt, etc... */
+#ifdef CONFIG_PPC64
 	lmb_reserve(0, __end_interrupts - _stext);
 	lmb_reserve(__pa(__end_interrupts),
 		    klimit - (unsigned long)__end_interrupts);
+#else
+	lmb_reserve(PHYSICAL_START, __pa(klimit) - PHYSICAL_START);
+#endif
 	reserve_kdump_trampoline();
 	reserve_crashkernel();
 	early_reserve_mem();
