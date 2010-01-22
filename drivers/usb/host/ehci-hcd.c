56f3714743b415f171835ec55f12eba8538ac084
Geoff Levand <geoffrey.levand@am.sony.com>
2010-01-22 11:53:33 -0800

    [PS3] Add ps3-wip/ps3-usb-fix-244.patch

--- drivers/usb/host/ehci-hcd.c
+++ drivers/usb/host/ehci-hcd.c
@@ -572,6 +572,7 @@ static int ehci_init(struct usb_hcd *hcd)
 	hw->hw_next = QH_NEXT(ehci, ehci->async->qh_dma);
 	hw->hw_info1 = cpu_to_hc32(ehci, QH_HEAD);
 	hw->hw_info1 |= cpu_to_hc32(ehci, (2 << 28));	/* RL = 2 */
+	hw->hw_info1 |= cpu_to_hc32(ehci, (1 << 7));	/* I = 1 */
 	hw->hw_token = cpu_to_hc32(ehci, QTD_STS_HALT);
 	hw->hw_qtd_next = EHCI_LIST_END(ehci);
 	ehci->async->qh_state = QH_STATE_LINKED;
