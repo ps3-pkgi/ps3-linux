1e30aace4545a7ea0d7be331a487072052e1b144
Geoff Levand <geoff@infradead.org>
2011-11-09 16:08:39 -0800

    ps3-debugging: Setup DABR register
    For debugging only.
    
    Helper routine to setup DABR (Data Address Breakpoint) register
    for kernel use.

--- arch/powerpc/include/asm/ps3.h
+++ arch/powerpc/include/asm/ps3.h
@@ -526,4 +526,13 @@ u32 ps3_get_and_clear_pm_interrupts(u32 cpu);
 void ps3_sync_irq(int node);
 u32 ps3_get_hw_thread_id(int cpu);
 
+/* kernel debug routines */
+
+#define   DABR_TRANSLATION	(1UL << 2)
+#define   DABR_DATA_WRITE	(1UL << 1)
+#define   DABR_DATA_READ	(1UL << 0)
+
+int ps3_debug_setup_dabr(u64 address, unsigned int dabr_flags,
+	unsigned int dabrx);
+
 #endif
