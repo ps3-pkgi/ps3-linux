789c20c82ff5ac38f20489fc407ea7b27ab10167
Geoff Levand <geoff@infradead.org>
2016-11-21 10:29:44 -0800

    ps3-debugging: Setup DABR register
    For debugging only.
    
    Helper routine to setup DABR (Data Address Breakpoint) register
    for kernel use.

--- arch/powerpc/mm/fault.c
+++ arch/powerpc/mm/fault.c
@@ -257,7 +257,8 @@ int do_page_fault(struct pt_regs *regs, unsigned long address,
 	 * The kernel should never take an execute fault nor should it
 	 * take a page fault to a kernel address.
 	 */
-	if (!user_mode(regs) && (is_exec || (address >= TASK_SIZE))) {
+	if (!user_mode(regs) && (is_exec || (address >= TASK_SIZE)) &&
+		!(error_code & DSISR_DABRMATCH)) {
 		rc = SIGSEGV;
 		goto bail;
 	}
