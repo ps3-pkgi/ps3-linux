fbf03b708d4dc54cc8d09508d389db20d8b67f5f
Geoff Levand <geoff@infradead.org>
2016-11-21 10:29:44 -0800

    ps3-debugging: Setup DABR register
    For debugging only.
    
    Helper routine to setup DABR (Data Address Breakpoint) register
    for kernel use.

--- arch/powerpc/kernel/process.c
+++ arch/powerpc/kernel/process.c
@@ -610,6 +610,9 @@ void do_break (struct pt_regs *regs, unsigned long address,
 {
 	siginfo_t info;
 
+	printk("%s:%d: address %016lxh, code %08lxh\n",
+		__func__, __LINE__, address, error_code);
+
 	current->thread.trap_nr = TRAP_HWBKPT;
 	if (notify_die(DIE_DABR_MATCH, "dabr_match", regs, error_code,
 			11, SIGSEGV) == NOTIFY_STOP)
@@ -620,6 +623,13 @@ void do_break (struct pt_regs *regs, unsigned long address,
 
 	/* Clear the breakpoint */
 	hw_breakpoint_disable();
+
+	if (!user_mode(regs)) {
+		show_regs(regs);
+		printk("%s:%d: done: address %016lxh\n", __func__, __LINE__,
+		address);
+		return;
+	}
 
 	/* Deliver the signal to userspace */
 	info.si_signo = SIGTRAP;
