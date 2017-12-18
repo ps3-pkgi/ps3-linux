20e8484fa412c75c48a5bc7864b63b33bcbea7ee
Geoff Levand <geoff@infradead.org>
2017-12-18 12:06:41 -0800

    powerpc/ps3: Add unused attribute
    Fixes build error with GCC-6.4.
    
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- arch/powerpc/platforms/ps3/os-area.c
+++ arch/powerpc/platforms/ps3/os-area.c
@@ -194,7 +194,7 @@ static const struct os_area_db_id os_area_db_id_rtc_diff = {
 	.key = OS_AREA_DB_KEY_RTC_DIFF
 };
 
-static const struct os_area_db_id os_area_db_id_video_mode = {
+static const __attribute__ ((unused)) struct os_area_db_id os_area_db_id_video_mode = {
 	.owner = OS_AREA_DB_OWNER_LINUX,
 	.key = OS_AREA_DB_KEY_VIDEO_MODE
 };
