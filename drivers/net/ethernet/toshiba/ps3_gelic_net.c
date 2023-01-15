72453410cbc41791f0a3f8d56bb8a0f0fdc8afc4
Geoff Levand <geoff@infradead.org>
2023-01-14 17:27:36 -0800

    net/ps3_gelic: Add new routine gelic_unmap_link
    Put the common code for unmaping a link into its own routine,
    gelic_unmap_link, and add some debugging checks.
    
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- drivers/net/ethernet/toshiba/ps3_gelic_net.c
+++ drivers/net/ethernet/toshiba/ps3_gelic_net.c
@@ -285,6 +285,21 @@ void gelic_card_down(struct gelic_card *card)
 	mutex_unlock(&card->updown_lock);
 }
 
+static void gelic_unmap_link(struct device *dev, struct gelic_descr *descr)
+{
+	BUG_ON(descr->hw_regs.payload.dev_addr);
+	BUG_ON(descr->hw_regs.payload.size);
+
+	BUG_ON(!descr->link.cpu_addr);
+	BUG_ON(!descr->link.size);
+
+	dma_unmap_single(dev, descr->link.cpu_addr, descr->link.size,
+		DMA_BIDIRECTIONAL);
+
+	descr->link.cpu_addr = 0;
+	descr->link.size = 0;
+}
+
 /**
  * gelic_card_free_chain - free descriptor chain
  * @card: card structure
@@ -297,9 +312,7 @@ static void gelic_card_free_chain(struct gelic_card *card,
 	struct gelic_descr *descr;
 
 	for (descr = descr_in; descr && descr->link.cpu_addr; descr = descr->next) {
-		dma_unmap_single(dev, descr->link.cpu_addr, descr->link.size,
-			DMA_BIDIRECTIONAL);
-		descr->link.cpu_addr = 0;
+		gelic_unmap_link(dev, descr);
 	}
 }
 
@@ -361,9 +374,7 @@ static int gelic_card_init_chain(struct gelic_card *card,
 iommu_error:
 	for (i--, descr--; 0 <= i; i--, descr--)
 		if (descr->link.cpu_addr)
-			dma_unmap_single(dev, descr->link.cpu_addr,
-					 descr->link.size,
-					 DMA_BIDIRECTIONAL);
+			gelic_unmap_link(dev, descr);
 	return -ENOMEM;
 }
 
