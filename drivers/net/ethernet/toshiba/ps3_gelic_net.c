17fb86de36a81aa53c75a2a55ed8eac08c13b254
Geoff Levand <geoff@infradead.org>
2023-02-05 11:02:33 -0800

    net/ps3_gelic_net: Rename to descr_count
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- drivers/net/ethernet/toshiba/ps3_gelic_net.c
+++ drivers/net/ethernet/toshiba/ps3_gelic_net.c
@@ -298,7 +298,7 @@ static void gelic_card_free_chain(struct gelic_card *card,
  * @card: card structure
  * @chain: address of chain
  * @start_descr: address of descriptor array
- * @no: number of descriptors
+ * @descr_count: number of descriptors
  *
  * we manage a circular list that mirrors the hardware structure,
  * except that the hardware uses bus addresses.
@@ -307,16 +307,16 @@ static void gelic_card_free_chain(struct gelic_card *card,
  */
 static int gelic_card_init_chain(struct gelic_card *card,
 				 struct gelic_descr_chain *chain,
-				 struct gelic_descr *start_descr, int no)
+				 struct gelic_descr *start_descr, int descr_count)
 {
 	struct device *dev = ctodev(card);
 	struct gelic_descr *descr;
 	int i;
 
-	memset(start_descr, 0, no * sizeof(*start_descr));
+	memset(start_descr, 0, descr_count * sizeof(*start_descr));
 
 	/* set up the hardware pointers in each descriptor */
-	for (i = 0, descr = start_descr; i < no; i++, descr++) {
+	for (i = 0, descr = start_descr; i < descr_count; i++, descr++) {
 		gelic_descr_set_status(descr, GELIC_DESCR_DMA_NOT_IN_USE);
 		descr->bus_addr =
 			dma_map_single(dev, descr,
@@ -342,8 +342,7 @@ static int gelic_card_init_chain(struct gelic_card *card,
 	start_descr->prev = (descr - 1);
 
 	/* chain bus addr of hw descriptor */
-	descr = start_descr;
-	for (i = 0; i < no; i++, descr++) {
+	for (i = 0, descr = start_descr; i < descr_count; i++, descr++) {
 		descr->next_descr_addr = cpu_to_be32(descr->next->bus_addr);
 	}
 
